d746be4dd939178795550b924d5c1875111c768a
DivijaBabu <divijababu@piktorlabs.com>
2022-09-13 20:59:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import JSONDATA from "./MOCK_DATA.json";
+import { useState,useMemo ,useEffect} from 'react';
+import debounce from 'lodash.debounce';
+import "./App.css";
 function App() {
+  const [searchWord, setSearchWord] = useState("");//allows us to track state 
+  const [datas,setData]=useState(JSONDATA);
+  const handleChange = (e) => {
+    setSearchWord(e.target.value); //event handler for the target element within our React application
+  };
+  const debounceD = useMemo(() => {//returns a memoized value
+    return debounce(handleChange, 900);
+  }, []);
+  useEffect(()=>{
+   let inputs = JSONDATA.filter((val) => {
+      if (val.first_name.toUpperCase().includes(searchWord.toUpperCase())) {            
+       return val;
+       }
+       else if (searchWord === "") {
+         return val;
+       }})
+       setData(inputs);
+  },[searchWord])
+  useEffect(() => {
+    return () => {
+      debounceD.cancel();
+    }; });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+    <center>
+      <div className="App">
+        <input id="searchbar" type="text" placeholder='search by name'onChange={debounceD}/>
+        {
+        datas.map((val, key) => {
+          if(searchWord){
+          let splitinput = val.first_name.split(searchWord)
+          return <div><p>{splitinput[0]}<em>{searchWord}</em>{splitinput[1]}</p></div>
+          }
+          else {
+            return <div><p>{val.first_name}</p></div>
+          }
+        })}
+      </div>
+    </center>
+  );}
 export default App;
+// useMemo optimization technique for accelerating computer programs by caching 
+// the results of heavy function calls and returning them when similar inputs are
+//  encountered repeatedly
